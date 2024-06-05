fea13515413febbce4e1e3f09e47deace08a2779
TheNonEconomist <thenoneconomist@gmail.com>
2024-06-05 18:46:23 -0400

    template and api tests

---
+++ factory.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+from web3 import Web3
+import argparse
+from story_protocol_python_sdk import StoryClient
+
+
+def main(args):
+    pass
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument()
+
+    main(parser.parse_args())
