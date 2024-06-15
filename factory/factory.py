f7c550ba94da566857790eb0115a978314ae433a
TheNonEconomist <thenoneconomist@gmail.com>
2024-06-15 18:31:19 -0400

    padding function and background color finding

--- factory/factory.py
+++ factory/factory.py
@@ -3,23 +3,51 @@ from dotenv import load_dotenv
 from web3 import Web3
 import argparse
 from story_protocol_python_sdk import StoryClient
-
-def emotion_and_situation_to_picture():
-    pass
-
-def emotion_and_situation_to_text():
-    pass
+import json
+import cv2
+import numpy as np
 
 
+def emotion_and_situation_to_picture(emotion, situation, materials):
+    for item in materials["pic_path"]:
+        if item["emotion"] == emotion and item["situation"] == situation:
+            return cv2.imread(item["text"]) 
 
-def emotion_to_situation():
-    pass
+def emotion_and_situation_to_top_text(emotion, situation, materials):
+    for item in materials["top_text"]:
+        if item["emotion"] == emotion and item["situation"] == situation: 
+            return item["text"]
 
-def situation_to_emotion():
-    pass
+def emotion_and_situation_to_bottom_text(emotion, situation, materials):
+    for item in materials["bottom_text"]:
+        if item["emotion"] == emotion and item["situation"] == situation: 
+            return item["text"]
+
+
+def emotion_to_situation(emotion, emotion_to_situation_mapping):
+    return emotion_to_situation_mapping[emotion]
+
+def situation_to_emotion(situation, situation_to_emotion_mapping):
+    return situation_to_emotion_mapping[situation]
 
 
 def main(args):
+    load_dotenv()
+    CURRENT_PATH = os.getenv('CURRENT_PATH')
+    with open(CURRENT_PATH + "/factory/context/on_chain_security/materials.json", "r") as f:
+        materials = json.load(f)
+        print(materials)
+
+    pic = emotion_and_situation_to_picture(args.emotion, args.situation, materials)
+    top_text = emotion_and_situation_to_top_text(args.emotion, args.situation, materials)
+    bottom_text = emotion_and_situation_to_bottom_text(args.emotion, args.situation, materials)
+
+    # TODO :top text -> add it to background
+    # TODO: bottom text -> add it to background
+    # TODO: background generator -> grab color from the pic that's on the background and create pic of a particular size
+    # -> approach 1. find the most common color combinations and cross ref with positioning (take top 1 or 2 and cross ref with corners)
+    # -> 
+
     pass
 # TODO: factory has 3 Components - 
 # 1) the picture part (queried via emotions and situation)
@@ -30,7 +58,8 @@ def main(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument()
+    parser.add_argument("-e", "--emotion", type=str)
+    parser.add_argument("-s", "--situation", type=str)
 
     main(parser.parse_args())
 
--- factory/factory_widgets.py
+++ factory/factory_widgets.py
@@ -1,9 +1,17 @@
 import cv2
 import numpy as np
 
-def concatenate_images_opencv(image_paths, horizontal=True):
+def concatenate_images_paths(image_paths, horizontal=True):
     images = [cv2.imread(img_path) for img_path in image_paths]
     
+    if horizontal:
+        concatenated_image = np.hstack(images)
+    else:
+        concatenated_image = np.vstack(images)
+    
+    return concatenated_image
+
+def concatenate_images_inline(images, horizontal=True):
     if horizontal:
         concatenated_image = np.hstack(images)
     else:
@@ -39,4 +47,45 @@ def __add_text_to_image(image_path, text, position, font=cv2.FONT_ITALIC, font_s
 
     return image
 
+def grab_background_color(image, approach):
+    h, w, _ = image.shape
+    majority_vote = dict()
+    if approach == "corners_and_edges":
+        for i in range(h):
+            try:
+                majority_vote[tuple(image[i, 0, :])] += 1
+            except KeyError:
+                majority_vote[tuple(image[i, 0, :])] = 1
 
+            try:
+                majority_vote[tuple(image[i, -1, :])] += 1
+            except KeyError:
+                majority_vote[tuple(image[i, -1, :])] = 1
+                 
+        for j in range(1, w-1):
+            try:
+                majority_vote[tuple(image[0, j, :])] += 1
+            except KeyError:
+                majority_vote[tuple(image[0, j, :])] = 1
+
+            try:
+                majority_vote[tuple(image[-1, j, :])] += 1
+            except KeyError:
+                majority_vote[tuple(image[-1, j, :])] = 1
+    
+    max_count, max_combi = 0, None
+    for color_combination, count in majority_vote.items():
+        if count > max_count: 
+            max_count= count
+            max_combi = color_combination
+    return max_combi
+
+
+def create_pad(color, height, width, depth=3):
+    return np.ones((height, width, depth))*color
+
+def add_padding_for_text(img, approach, original_to_pad_height_ratio):
+    pad_color = grab_background_color(img, approach)
+    h, w, d = img.shape
+    padding = create_pad(pad_color, h//original_to_pad_height_ratio, w, d)
+    return concatenate_images_inline(images=[padding, img, padding], horizontal=False)
